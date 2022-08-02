c6120bdfecb50482850de8d74131ba0da1ad0ba5
Kristijan Kovacevic <44801324+kristijan10@users.noreply.github.com>
2022-08-02 16:30:20 +0200

    Updates

---
+++ precache-manifest.a1cfa2e50ddaa580143da82adafcb6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbaec86e79b85e69ce61",
+    "url": "/robofriends/static/css/main.26244859.chunk.css"
+  },
+  {
+    "revision": "bbaec86e79b85e69ce61",
+    "url": "/robofriends/static/js/main.bbaec86e.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/robofriends/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "274396811fff8c54c05e",
+    "url": "/robofriends/static/js/2.27439681.chunk.js"
+  },
+  {
+    "revision": "a433ede44710983729ce",
+    "url": "/robofriends/static/js/runtime~main.a433ede4.js"
+  },
+  {
+    "revision": "46e6af93b8bc3ea36983e76f64657705",
+    "url": "/robofriends/index.html"
+  }
+];
